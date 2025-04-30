b3408503a5ba577136202ac648dcf455f1c50be2
lihuibear <lihuibear4@gmail.com>
2025-04-30 19:20:20 +0800

    day1

---
+++ src/main/java/com/lihui/aiagent/demo/invok/LangChainAiInvoke.java
@@ -0,0 +1,16 @@
+package com.lihui.aiagent.demo.invok;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+
+public class LangChainAiInvoke {
+
+    public static void main(String[] args) {
+        ChatLanguageModel qwenChatModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.API_KEY)
+                .modelName("qwen-max")
+                .build();
+        String answer = qwenChatModel.chat("我是lihuibear");
+        System.out.println(answer);
+    }
+}
