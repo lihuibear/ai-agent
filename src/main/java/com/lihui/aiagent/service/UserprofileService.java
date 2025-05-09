ffeeadf58c5add732efd67e7a3b925313381303b
lihuibear <lihuibear4@gmail.com>
2025-05-09 12:01:24 +0800

    fix:bug RAG基础-扩展-AI丘比特（媒婆）-数据库实现

---
+++ src/main/java/com/lihui/aiagent/service/UserprofileService.java
@@ -0,0 +1,14 @@
+package com.lihui.aiagent.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lihui.aiagent.domain.Userprofile;
+
+/**
+* @author lihui
+* @description 针对表【userprofile】的数据库操作Service
+* @createDate 2025-05-08 18:31:09
+*/
+public interface UserprofileService extends IService<Userprofile> {
+
+}
---
+++ src/main/java/com/lihui/aiagent/service/impl/UserprofileServiceImpl.java
@@ -0,0 +1,24 @@
+package com.lihui.aiagent.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lihui.aiagent.domain.Userprofile;
+import com.lihui.aiagent.mapper.UserprofileMapper;
+import com.lihui.aiagent.service.UserprofileService;
+
+
+import org.springframework.stereotype.Service;
+
+/**
+* @author lihui
+* @description 针对表【userprofile】的数据库操作Service实现
+* @createDate 2025-05-08 18:31:09
+*/
+@Service
+public class UserprofileServiceImpl extends ServiceImpl<UserprofileMapper, Userprofile>
+    implements UserprofileService {
+
+}
+
+
+
+
